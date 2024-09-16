217311f96846ea88322737bc180ce2dca4dcc148
Gustavo <romerogustavob@gmail.com>
2024-09-16 15:04:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//})
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
 export default defineConfig({
-  server: {
-    port: process.env.PORT as unknown as number,
-  },
-  plugins: [vercel()],
-});
+  plugins: [react()],
+})
