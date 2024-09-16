10ecac220e5dbfecca3e889ed80f67b758e88a43
JulianCodina <depedrojulianismael@gmail.com>
2024-09-16 13:45:17 -0300

    desarroladores

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -51,39 +51,53 @@ export default function Footer() {
   return (
     <div className="footer" id="contact">
       <div className="contact">
-        <h3>CONTÁCTANOS</h3>
-        <div className="item">
-          <img src="/src/assets/ubicacion.png" alt="Ubicacion" />
-          <p>ANJ, Saavedra 449, H3500 Resistencia, Provincia del Chaco</p>
-        </div>
-        <div className="item">
-          <img src="/src/assets/gmail.png" alt="Gmail" />
-          <p>correoelectronico@email.com</p>
-        </div>
-        <div className="item">
-          <img src="/src/assets/whatsapp.png" alt="Whatsapp" />
-          <p>+54 3624242424</p>
-        </div>
-        <div className="item">
-          <img src="/src/assets/facebook.png" alt="facebook" />
-          <p>@Sentirse.bien</p>
-        </div>
-        <div className="itemEmpleo">
-          <h3>EMPLEO</h3>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <div className="archivo">
-              <input
-                type="file"
-                accept=".pdf,.jpg,.jpeg,.png"
-                onChange={handleFileChange}
-                name="file" // Necesario para enviar el archivo
-              />
-              <input className="submit" type="submit" value="Enviar CV" />
+        <div className="par">
+          <div className="contactInfo">
+            <h3>CONTÁCTANOS</h3>
+            <div className="item">
+              <img src="/src/assets/ubicacion.png" alt="Ubicacion" />
+              <p>ANJ, Saavedra 449, H3500 Resistencia, Provincia del Chaco</p>
             </div>
-          </form>
+            <div className="item">
+              <img src="/src/assets/gmail.png" alt="Gmail" />
+              <p>correoelectronico@email.com</p>
+            </div>
+            <div className="item">
+              <img src="/src/assets/whatsapp.png" alt="Whatsapp" />
+              <p>+54 3624242424</p>
+            </div>
+            <div className="item">
+              <img src="/src/assets/facebook.png" alt="facebook" />
+              <p>@Sentirse.bien</p>
+            </div>
+          </div>
+          <GoogleMap />
+        </div>
+        <div className="par">
+          <div className="itemEmpleo">
+            <h3>EMPLEO</h3>
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <div className="archivo">
+                <input
+                  type="file"
+                  accept=".pdf,.jpg,.jpeg,.png"
+                  onChange={handleFileChange}
+                  name="file" // Necesario para enviar el archivo
+                />
+                <input className="submit" type="submit" value="Enviar CV" />
+              </div>
+            </form>
+          </div>
+          <div className="itemEmpleo">
+            <h3>DESARROLLADORES</h3>
+            <a href="https://github.com/JulianCodina">Julian Codina</a>
+            <br />
+            <a href="https://github.com/AstridV23">Astrid Viñuela</a>
+            <br />
+            <a href="https://github.com/exequielcabrera98">Exequiel Cabrera</a>
+          </div>
         </div>
       </div>
-      <GoogleMap />
     </div>
   );
 }
